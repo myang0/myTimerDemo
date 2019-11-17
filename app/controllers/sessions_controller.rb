96ee5e9d31a332f9d6b569a59db2305ecc88d75c
myang0 <mwyang99@gmail.com>
2019-11-17 14:54:17 -0800

    added basic login/logout functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+        @user = User.find_by_uname(params[:session][:uname])
+        if @user && @user.authenticate(params[:session][:password])
+            session[:user_id] = @user.id
+            redirect_to '/welcome'
+        else
+            redirect_to 'login'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        redirect_to '/'
+    end
+
+end
