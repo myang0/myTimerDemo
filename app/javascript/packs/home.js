a84e5447864eeba740dc9fed922dbc49ec9ec241
Matthew Yang <mwyang99@gmail.com>
2019-12-11 04:26:09 -0800

    changed homepage UI

---
+++ app/javascript/packs/home.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('#ss-btn').mouseenter(function() {
+        $('#ss-text').slideDown();
+    })
+
+    $('#ss-btn').mouseleave(function() {
+        $('#ss-text').slideUp();
+    })
+
+    $('#st-btn').mouseenter(function() {
+        $('#st-text').slideDown();
+    })
+
+    $('#st-btn').mouseleave(function() {
+        $('#st-text').slideUp();
+    })
+
+    $('#ach-btn').mouseenter(function() {
+        $('#ach-text').slideDown();
+    })
+
+    $('#ach-btn').mouseleave(function() {
+        $('#ach-text').slideUp();
+    })
+
+    $('#gl-btn').mouseenter(function() {
+        $('#gl-text').slideDown();
+    })
+
+    $('#gl-btn').mouseleave(function() {
+        $('#gl-text').slideUp();
+    })
+})
