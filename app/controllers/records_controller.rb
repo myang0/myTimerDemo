74f61b26c3d9e0070cbd0565c275b2991a7c4af5
Matthew Yang <mwyang99@gmail.com>
2019-12-11 23:53:47 -0800

    changed sorting of session stats

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -8,48 +8,48 @@ class RecordsController < ApplicationController
 
         # All records from today to a year ago
         if request.fullpath == '/stats/year'
-            @successes = Record.group_by_month(:created_at, reverse: false).where(uname: session[:user_name],
+            @successes = Record.group_by_month_of_year(:created_at, reverse: false).where(uname: session[:user_name],
                                                                                  created_at: (t - 365.day)..t,
                                                                                  success: true).sum(:duration)
 
-            @failures = Record.group_by_month(:created_at, reverse: false).where(uname: session[:user_name],
+            @failures = Record.group_by_month_of_year(:created_at, reverse: false).where(uname: session[:user_name],
                                                                                 created_at: (t - 365.day)..t,
                                                                                 success: false).sum(:duration)
         # All records from today to a month ago
         elsif request.fullpath == '/stats/month'
-            @successes = Record.group_by_day(:created_at, reverse: false).where(uname: session[:user_name],
+            @successes = Record.group_by_day_of_month(:created_at, reverse: false).where(uname: session[:user_name],
                                                                                created_at: (t - 30.day)..t,
                                                                                success: true).sum(:duration)
 
-            @failures = Record.group_by_day(:created_at, reverse: false).where(uname: session[:user_name],
+            @failures = Record.group_by_day_of_month(:created_at, reverse: false).where(uname: session[:user_name],
                                                                               created_at: (t - 30.day)..t,
                                                                               success: false).sum(:duration)
         # All records from today to a week ago
         elsif request.fullpath == '/stats/week'
-            @successes = Record.group_by_day(:created_at, reverse: false).where(uname: session[:user_name], 
+            @successes = Record.group_by_day_of_week(:created_at, reverse: false).where(uname: session[:user_name], 
                                                                                created_at: (t - 7.day)..t,
                                                                                success: true).sum(:duration)
 
-            @failures = Record.group_by_day(:created_at, reverse: false).where(uname: session[:user_name], 
+            @failures = Record.group_by_day_of_week(:created_at, reverse: false).where(uname: session[:user_name], 
                                                                               created_at: (t - 7.day)..t,
                                                                               success: false).sum(:duration)
 
         # All records from today
         elsif request.fullpath == '/stats/day'
-            @successes = Record.group_by_hour(:created_at, reverse: false).where(uname: session[:user_name],
+            @successes = Record.group_by_hour_of_day(:created_at, reverse: false).where(uname: session[:user_name],
                                                                                 created_at: (t - 1.day)..t,
                                                                                 success: true).sum(:duration)
 
-            @failures = Record.group_by_hour(:created_at, reverse: false).where(uname: session[:user_name],
+            @failures = Record.group_by_hour_of_day(:created_at, reverse: false).where(uname: session[:user_name],
                                                                                created_at: (t - 1.day)..t,
                                                                                success: false).sum(:duration)
         # Default to all records from today to a week ago
         else
-            @successes = Record.group_by_day(:created_at, reverse: false).where(uname: session[:user_name], 
+            @successes = Record.group_by_day_of_week(:created_at, reverse: false).where(uname: session[:user_name], 
                                                                                created_at: (t - 7.day)..t,
                                                                                success: true).sum(:duration)
                                                               
-            @failures = Record.group_by_day(:created_at, reverse: false).where(uname: session[:user_name], 
+            @failures = Record.group_by_day_of_week(:created_at, reverse: false).where(uname: session[:user_name], 
                                                                               created_at: (t - 7.day)..t,
                                                                               success: false).sum(:duration)
         end
